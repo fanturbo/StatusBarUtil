485fa188fc5e541b71bce8effa789995735b7727
turbofan <615951217@qq.com>
2017-11-14 11:24:04 +0800

    白底黑字适配

--- sample/src/main/java/com/jaeger/statusbarutil/UseInFragmentActivity.java
+++ sample/src/main/java/com/jaeger/statusbarutil/UseInFragmentActivity.java
@@ -1,20 +1,27 @@
 package com.jaeger.statusbarutil;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.jaeger.library.StatusBarUtil;
 import com.jaeger.statusbardemo.R;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by Jaeger on 16/8/11.
- *
+ * <p>
  * Email: chjie.jaeger@gmail.com
  * GitHub: https://github.com/laobie
  */
@@ -22,6 +29,7 @@ public class UseInFragmentActivity extends BaseActivity {
     private ViewPager mVpHome;
     private BottomNavigationBar mBottomNavigationBar;
     private ArrayList<Fragment> mFragmentList = new ArrayList<>();
+    private int mPosition;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,10 +38,10 @@ public class UseInFragmentActivity extends BaseActivity {
         mVpHome = (ViewPager) findViewById(R.id.vp_home);
         mBottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
         mBottomNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_favorite, "One"))
-            .addItem(new BottomNavigationItem(R.drawable.ic_gavel, "Two"))
-            .addItem(new BottomNavigationItem(R.drawable.ic_grade, "Three"))
-            .addItem(new BottomNavigationItem(R.drawable.ic_group_work, "Four"))
-            .initialise();
+                .addItem(new BottomNavigationItem(R.drawable.ic_gavel, "Two"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_grade, "Three"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_group_work, "Four"))
+                .initialise();
 
         mBottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
             @Override
@@ -65,6 +73,7 @@ public class UseInFragmentActivity extends BaseActivity {
 
             @Override
             public void onPageSelected(int position) {
+                mPosition = position;
                 mBottomNavigationBar.selectTab(position);
                 switch (position) {
                     case 0:
@@ -99,6 +108,52 @@ public class UseInFragmentActivity extends BaseActivity {
 
     @Override
     protected void setStatusBar() {
-        StatusBarUtil.setTranslucentForImageViewInFragment(UseInFragmentActivity.this, null);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if ("Xiaomi".equals(Build.MANUFACTURER)) {
+                if (mPosition == 0) {
+                    Window window = getWindow();
+                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        window.setStatusBarColor(Color.TRANSPARENT);
+                        window.getDecorView()
+                                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                    }
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                        CommonUtil.StatusBarLightMode(this, 1);
+                    }
+                } else {
+                    Window window = this.getWindow();
+                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        window.setStatusBarColor(Color.TRANSPARENT);
+                        window.getDecorView()
+                                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                    }
+                    StatusBarUtil.setColor(this, getResources().getColor(R.color.white), 0);
+                    CommonUtil.StatusBarLightMode(this, 1);
+                }
+            } else if ("MEIZU".equals(Build.MANUFACTURER)) {
+                //识别魅族手机 测试下魅族5.0的手机
+                if (mPosition == 0) {
+                    StatusBarUtil.setTranslucentForImageViewInFragment(this, 0, null);
+                } else {
+                    StatusBarUtil.setColor(this, Color.WHITE, 0);
+                }
+                CommonUtil.StatusBarLightMode(this, 2);
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (mPosition == 0) {
+                    StatusBarUtil.setTranslucentForImageViewInFragment(this, 0, null);
+                } else {
+                    StatusBarUtil.setColor(this, Color.WHITE, 0);
+                }
+                CommonUtil.StatusBarLightMode(this, 3);
+            } else {
+                StatusBarUtil.setTranslucentForImageViewInFragment(this, 0, null);
+            }
+        } else {
+            StatusBarUtil.setTranslucentForImageViewInFragment(this, 0, null);
+        }
     }
 }
